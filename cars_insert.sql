d58fcc2a80abe8b48b0a6a3ea57e0e8bd89ebe3d
Wronqa <87782832+Wronqa@users.noreply.github.com>
2021-12-29 21:11:19 +0100

    Add files via upload

---
+++ cars_insert.sql
@@ -0,0 +1,16 @@
+--OSOBA insert
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(1,'Cięzki wóz', 'TOS2137', 1);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(2,'Cięzki wóz', 'TOS2565', 2);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(3,'Cięzki wóz', 'TOS5644', 3);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(4,'Cięzki wóz', 'TOS4567', 4);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(5,'Cięzki wóz', 'TOS4566', 5);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(6,'Lekki wóz', 'TK56464', 6);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(7,'Lekki wóz', 'TK55676', 7);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(8,'Lekki wóz', 'TK562344', 8);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(9,'Lekki wóz', 'TK562344', 9);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(10,'Lekki wóz', 'TK523434', 10);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(11,'Wóz zaopatrzeniowy', 'WW56456', 11);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(12,'Jeep terenowy', 'TOP4356', 12);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(13,'Jeep terenowy', 'TST4564', 13);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(14,'Jeep terenowy', 'TOP45466', 14);
+INSERT INTO Pojazd(ID_POJAZDU, Rodzaj, Numer_Rejestracyjny,ID_UBEZPIECZENIA) VALUES(15,'Jeep terenowy', 'TOP42346', 15);
