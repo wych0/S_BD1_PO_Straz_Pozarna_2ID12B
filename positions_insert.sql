bd57d82cb32c5792df57f34ab2fb2a3ca526c7af
Wronqa <87782832+Wronqa@users.noreply.github.com>
2021-12-29 20:58:34 +0100

    Add files via upload

---
+++ positions_insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(1,'Strażak');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(2,'Woźny');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(3,'Dyrektor');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(4,'Menadzer');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(5,'Technik');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(6,'Kierowca');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(7,'Sekretarka');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(8,'Księgowa');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(9,'Dyżurny');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(10,'Strażak');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(11,'Kierownik');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(12,'Zastępca kierownika');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(13,'Ochroniarz');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(14,'Specjalista');
+INSERT INTO Stanowisko(ID_STANOWISKA,Nazwa) VALUES(15,'Zastępca kierownika');
