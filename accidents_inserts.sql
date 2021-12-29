6568f22856fe49b16139deb271e4718c660b0c8b
Wronqa <87782832+Wronqa@users.noreply.github.com>
2021-12-29 21:25:16 +0100

    Add files via upload

---
+++ accidents_inserts.sql
@@ -0,0 +1,29 @@
+--STRAZAK insert
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(1,'2021-03-17','Palil sie dom', 1,1,1,1);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(2,'2021-04-05
+','Wybuch gazu', 2,2,2,2);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(3,'2021-04-14
+','Kot na drzewie', 3,3,3,3);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(4,'2021-05-04
+','Styrta sie dopala', 4,4,4,4);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(5,'2021-07-20
+','Wypadek samochodowy', 5,5,5,5);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(6,'2021-08-03
+','Pożar bloku', 6,6,6,6);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(7,'2021-08-20
+','Wyciek ropy na austradzie', 7,7,7,7);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(8,'2021-09-03
+','Bomba na uczelni', 8,8,8,8);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(9,'2021-09-17
+','Traba powietrzna zerwala dach', 9,9,9,9);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(10,'2021-10-15
+','Tlenek węgla sie ulatnial', 10,10,10,10);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(11,'2021-11-08','Wybuch gazu', 11,11,11,11);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(12,'2021-11-17
+','Wybuch gazu', 12,12,12,12);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(13,'2021-11-22
+','Wypadek samochodowyu', 13,13,13,13);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(14,'2021-12-10
+','Wypadek samochodowy', 14,14,14,14);
+INSERT INTO Zgloszenie(ID_ZGLOSZENIA, Data_zgloszenia, Opis, ID_ADRESU, ID_ODDZIALU, ID_POJAZDU, ID_SPRZETU) VALUES(15,'2021-12-17
+','Wypadek samochodowy', 15,15,15,15);
