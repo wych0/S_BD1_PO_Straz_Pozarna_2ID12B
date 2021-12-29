471469b83486fe4d7fef860883a2955b53c6cba8
Wronqa <87782832+Wronqa@users.noreply.github.com>
2021-12-29 21:22:56 +0100

    Add files via upload

---
+++ equipments_inserts.sql
@@ -0,0 +1,16 @@
+--SPRZET insert
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(1,'Siekierka');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(2,'Butla tlenowa');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(3,'Butla tlenowa');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(4,'Gaśnica');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(5,'Butla tlenowa');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(6,'Drabina');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(7,'Drabina');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(8,'Drabina');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(9,'Siekierka');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(10,'Maska tlenowa');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(11,'Gaśnica');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(12,'Maska tlenowa');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(13,'Młotek');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(14,'Młotek');
+INSERT INTO Sprzet(ID_SPRZETU, Nazwa) VALUES(15,'Gaśnica'); 
